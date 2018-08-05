aa8405b8851e2cf32c280962f306f24ee1c8ef24
mmihaylov82 <mmihaylov82@gmail.com>
2018-08-05 21:49:18 +0300

    Add 05. Entity Relations exercise

---
+++ 05. Entity Relations/01. Student System/P01_StudentSystem.Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data
+{
+    public class Configuration
+    {
+        public const string ConnectionString = @"Server=MMIHAYLOV10\SQLEXPRESS;Database=StudentSystem;Integrated Security=True;";
+    }
+}
---
+++ 05. Entity Relations/01. Student System/P01_StudentSystem.Data/EntityConfiguration/HomeworkConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P01_StudentSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data.EntityConfiguration
+{
+    public class HomeworkConfig : IEntityTypeConfiguration<Homework>
+    {
+        public void Configure(EntityTypeBuilder<Homework> builder)
+        {
+            builder.Property(x => x.Content)
+                 .IsUnicode(false);
+        }
+    }
+}
